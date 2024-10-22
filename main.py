77b78f2e9d8e20829e5a83beee9a6751020a23af
Busy86 <ivandembitsky@yandex.ru>
2024-10-22 16:30:05 +0500

    начал обучать

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 import torch.optim as optim
 
+from tqdm import tqdm
 import random
 import numpy as np
 from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.metrics import f1_score
 from PIL import Image
 
@@ -30,3 +32,101 @@ model = models.resnet18(pretrained=True)
 num_classes = 10
 model.fc = nn.Linear(model.fc.in_features, num_classes)
 
+train_data_dir = 'train2'
+val_data_dir = 'test'
+
+# Определим трансформации
+train_transforms = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor(),
+])
+
+val_transforms = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor(),
+])
+
+# Создадим датасеты
+train_dataset = ImageFolder(train_data_dir, transform=train_transforms)
+val_dataset = ImageFolder(val_data_dir, transform=val_transforms)
+
+# Создадим датагенераторы
+batch_size = 64
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+classes_list = train_dataset.classes
+
+# Определим функцию потерь и оптимизатор
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.0005)
+
+# число эпох
+num_epochs = 70
+
+train_losses = []
+train_accuracies = []
+val_losses = []
+val_accuracies = []
+best_val_accuracy = 0
+model.to(device)
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+    for images, labels in tqdm(train_loader):
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+
+    train_loss = running_loss / len(train_loader)
+    train_accuracy = correct / total
+    train_losses.append(train_loss)
+    train_accuracies.append(train_accuracy)
+
+    # Валидация модели
+    model.eval()
+    val_loss = 0.0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in tqdm(val_loader):
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            val_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+
+    val_loss /= len(val_loader)
+    val_accuracy = correct / total
+    val_losses.append(val_loss)
+    val_accuracies.append(val_accuracy)
+
+    print(f'Epoch [{epoch + 1}/{num_epochs}], '
+          f'Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.4f}, '
+          f'Val Loss: {val_loss:.4f}, Val Accuracy: {val_accuracy:.4f}')
+
+    # Сохранение лучшей модели на основе валидационной точности
+    if val_accuracy > best_val_accuracy:
+        best_val_accuracy = val_accuracy
+        torch.save(model.state_dict(), 'best_model.pth')
+        print('Saved best model!')
+
+    # Сохранение последней актуальной модели
+    torch.save(model.state_dict(), 'last_model.pth')
+    print()
+
+print('Training and validation complete!')
