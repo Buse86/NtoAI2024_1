eb1d8fada9be8cef19be44bbdd1c55e6a6b92956
Busy86 <ivandembitsky@yandex.ru>
2024-10-22 16:23:10 +0500

    сделал тест, начал обучать

--- data.py
+++ data.py
@@ -1,28 +1,31 @@
 import pandas as pd
 import shutil
 import os
-
-def InTrain():
-    for i in range(28015):
-        shutil.move(f'/train2/{df["image_name"].iloc[i]}', f'C:/Users/Geo/PycharmProjects/nto2024AI_1/train2/{a[df["class_id"].iloc[i]]}')
-    return None
-
-def InTest():
-    for i in os.listdir('train'):
-        x = os.listdir(f'train/{i}')
-        for j in range(len(x) // 10):
-            shutil.move(f'train/{i}/{x[j]}', f'test/{i}/{x[j]}')
-    return None
+import random
+# def InTrain(df, a):
+#     for i in range(28015):
+#         shutil.move(f'/train2/{df["image_name"].iloc[i]}', f'C:/Users/Geo/PycharmProjects/nto2024AI_1/train2/{a[df["class_id"].iloc[i]]}')
+#     return None
+#
+# def InTest():
+#     for i in os.listdir('train2'):
+#         x = os.listdir(f'train2/{i}')
+#         for j in range(len(x) // 10):
+#             shutil.move(f'train2/{i}/{x[j]}', f'test/{i}/{x[j]}')
+#     return None
 
 df = pd.read_csv('train.csv')
 df = df.drop(columns=['unified_class'])
 
 a = ['Заяц', 'Кабан', 'Кошки',  'Куньи', 'Медведь', 'Оленевые', 'Пантеры', 'Полорогие', 'Собачие', 'Сурок']
 
+# InTrain(df, a)
+#
 # for i in range(28015):
-#     shutil.move(f'/train2/{df["image_name"].iloc[i]}', f'C:/Users/Geo/PycharmProjects/nto2024AI_1/train2/{a[df["class_id"].iloc[i]]}')
-
-for i in os.listdir('train'):
-    x = os.listdir(f'train/{i}')
-    for j in range(len(x) // 10):
-        shutil.move(f'train/{i}/{x[j]}', f'test/{i}/{x[j]}')
+#     shutil.move(f'train/{df["image_name"].iloc[i]}',
+#                 f'train2/{a[df["class_id"].iloc[i]]}')
+#
+# for i in os.listdir('train2'):
+#     x = random.sample(os.listdir(f'train2/{i}'), len(os.listdir(f'train2/{i}')))
+#     for j in range(len(x) // 10):
+#         shutil.move(f'train2/{i}/{x[j]}', f'test/{i}/{x[j]}')
